90e305e66d1d1003c4d2cccc00e1d07bca794bed
Saidgani Dadajanov <dondiazglobal@gmail.com>
2024-09-03 11:52:12 -0400

    created some form and view

--- leads/views.py
+++ leads/views.py
@@ -1,12 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Lead
+from .models import Lead, Agent
+from .forms import LeadForm
 
 
-def home_page(request):
+def lead_list(request):
     leads = Lead.objects.all()
     context = {
         "leads":leads
     }
-    return render(request, "second_page.html", context)
+    return render(request, "leads/lead_list.html", context)
+
+
+def lead_detail(request, pk):
+    print(pk)
+    lead = Lead.objects.get(id=pk)
+    context = {
+        "lead": lead
+    }
+    return render(request, "leads/lead_detail.html", context)
+
+
+def lead_create(request):
+    print()
+    if request.method == "POST":
+        print("Reciving a post request")
+        form = LeadForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            age = form.cleaned_data['age']
+            agent = Agent.objects.first()
+            Lead.objects.create(
+                first_name=first_name,
+                last_name=last_name,
+                age=age,
+                agent=agent
+            )
+            return redirect("/leads")
+    context = {
+        "form": LeadForm()
+    }
+    return render(request, "leads/lead_create.html", context)
     
